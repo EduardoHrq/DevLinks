a9213bbe818ac5416f28a35e2dd1fd315f419201
Eduardo Henrique <eduardohenriq0612002@gmail.com>
2023-02-27 11:03:38 -0300

    ok

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import './styles/global.css'
 export const metadata = {
   title: 'Links',
   description: 'App de links',
+  icons: 'https://img.icons8.com/material-sharp/256/06b6d4/resume.png',
 }
 
 export default function RootLayout({ children }) {
