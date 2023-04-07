47ec36ed2ccbf074b70ef0df53c9dce978beb8dc
Eduardo Henrique <eduardohenriq0612002@gmail.com>
2023-04-07 15:58:46 -0300

    ok

--- src/app/components/icons.js
+++ src/app/components/icons.js
@@ -1,9 +1,9 @@
-function Icon({color, name, link}) {
-  const url = "https://img.icons8.com/material-sharp/256/"+ color + "/" + name + ".png";
+function Icon({ color, name, link }) {
+  const url = "https://img.icons8.com/material-sharp/256/" + color + "/" + name + ".png";
 
   return (
-    <a href={link} target="_blank" className="block rounded-full p-3 hover:bg-gray-700 transition">
-      <img src={url} alt="" className="w-12"/>
+    <a href={link} target="_blank" className="icons block rounded-full p-3 transition-all duration-300">
+      <img src={url} alt="" className="w-12" />
     </a>
   )
 }
