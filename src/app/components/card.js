c6f42b76a6cb85b1fa6d87c92308e632f3842648
Eduardo Henrique <eduardohenriq0612002@gmail.com>
2023-04-07 15:52:06 -0300

    ok

--- src/app/components/card.js
+++ src/app/components/card.js
@@ -1,3 +1,5 @@
+import './style.css'
+
 function Card({ title, link }) {
   return (
     <a href={link} target="_blank" className="card w-80 h-14 mt-6 m-auto rounded-md flex items-center justify-center font-bold transition-all duration-300 bg-indigo-500">{title}</a>
