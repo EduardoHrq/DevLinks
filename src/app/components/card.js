9ddcaa74468eb9545b6e2339753599f81c6415e0
Eduardo Henrique <eduardohenriq0612002@gmail.com>
2023-04-12 19:54:20 -0300

    Atualizar o card.js

--- src/app/components/card.js
+++ src/app/components/card.js
@@ -2,7 +2,7 @@ import './style.css'
 
 function Card({ title, link }) {
   return (
-    <a href={link} target="_blank" className="card w-80 h-14 mt-6 rounded-md flex items-center justify-center font-bold transition-all duration-300 bg-indigo-500">{title}</a>
+    <a href={link} target="_blank" className="card w-80 h-14 mt-6 rounded-md flex items-center justify-center text-center font-bold transition-all duration-300 bg-indigo-500">{title}</a>
   )
 }
 
